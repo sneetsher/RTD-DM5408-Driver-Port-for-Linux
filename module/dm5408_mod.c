773c896f4ad253a1e690e6b99525207fe4905d37
Abdellah Chelli <AbdellahChelli@gmail.com>
2010-11-30 23:23:00 +0100

    Adding more headers & module infos, plus I/O base address parameter

--- module/dm5408_mod.c
+++ module/dm5408_mod.c
@@ -1,18 +1,41 @@
-#include <linux/module.h>	
-#include <linux/kernel.h>	
+#include <linux/errno.h>
+#include <linux/init.h>
+#include <linux/ioport.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/sched.h>
+#include <linux/stat.h>
+#include <linux/types.h>
+#include <linux/version.h>
 
 
-MODULE_AUTHOR("Abdellah Chelli <abdellahchelli@gmail.com>");
-MODULE_LICENSE("LGPL");
+static ushort iobase = 0x0300;
 
 
-int init_module(void)
+static int __init initialization_function(void)
 {
-	printk(KERN_INFO "dm5408_init_module() called\n");
+	printk(KERN_INFO "DM5408 init_module() called with iobase=0x%04x.\n", iobase);
 	return 0;
 }
 
-void cleanup_module(void)
+
+static void __exit cleanup_function(void)
 {
-	printk(KERN_INFO "dm5408_cleanup_module() called\n");
+	printk(KERN_INFO "DM5408 cleanup_module() called.\n");
 }
+
+
+module_param(iobase, ushort, S_IRUGO);
+
+MODULE_PARM_DESC(iobase, "I/O port base address");
+
+MODULE_AUTHOR("Abdellah Chelli <abdellahchelli@gmail.com>");
+MODULE_DESCRIPTION("RTD DM5408 Acquisition Card Driver. (No IRQ/DMA Support)");
+MODULE_SUPPORTED_DEVICE("dm5408");
+MODULE_LICENSE("Dual BSD/GPL");
+MODULE_ALIAS("dm408_mod");
+
+
+module_init(initialization_function);
+module_exit(cleanup_function);
