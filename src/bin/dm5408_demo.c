f1d3aefc3caba297c4a634aa826e3e28c74d55f9
Abdellah Chelli (Sneetsher) <abdellahchelli@gmail.com>
2011-10-07 23:51:41 +0100

    Update demo program to use ncurses lib

--- src/bin/dm5408_demo.c
+++ src/bin/dm5408_demo.c
@@ -23,45 +23,120 @@
 
 #include <fcntl.h>
 #include <ncurses.h>
-
+#include <signal.h>
+#include <time.h>
 
 #include "libdm5408.h"
+
+static void finish(int);
 
 int main(int argc, char *argv[])
 {
   int file_desc;
   uchar test = 0;
+  bool cont = TRUE;
+  int count = 0;
 
-  // Opening dev file
   char dev_name[] =  DEV_NAME;
   strcat(dev_name, "0");
-  file_desc = open( dev_name, 0);
-  if (file_desc < 0)
+
+  /* initialize your non-curses data structures here */
+  (void) signal(SIGINT, finish);   /* arrange interrupts to terminate */
+
+  (void) initscr();     /* initialize the curses library */
+  keypad(stdscr, TRUE); /* enable keyboard mapping */
+  (void) nonl();        /* tell curses not to do NL->CR/NL on output */
+  (void) cbreak();      /* take input chars one at a time, no wait for \n */
+  (void) echo();        /* echo input - in color */
+
+  if (has_colors())
   {
-    printf("Can't open device file: %s.\n", dev_name);
-    exit(-1);
+    start_color();
+
+    /*
+     * Simple color assignment, often all we need.  Color pair 0 cannot
+     * be redefined.  This example uses the same value for the color
+     * pair as for the foreground color, though of course that is not
+     * necessary:
+     */
+    init_pair(1, COLOR_RED,     COLOR_BLACK);
+    init_pair(2, COLOR_GREEN,   COLOR_BLACK);
+    init_pair(3, COLOR_YELLOW,  COLOR_BLACK);
+    init_pair(4, COLOR_BLUE,    COLOR_BLACK);
+    init_pair(5, COLOR_CYAN,    COLOR_BLACK);
+    init_pair(6, COLOR_MAGENTA, COLOR_BLACK);
+    init_pair(7, COLOR_WHITE,   COLOR_BLACK);
   }
-  else
-    printf("Device file: %s opened.\n", dev_name);
 
-  test = read_status_register(file_desc);
-  printf("Status reg: 0x%02X.\n", test);
+  while (cont)
+  {
+    attrset(COLOR_PAIR(7));
+    mvaddch(0,0,'>');
+    mvprintw(0,5,"cmd #: %4d",count);
+    attrset(COLOR_PAIR(2));
+    int c = mvgetch(0,1);/* refresh, accept single keystroke of input */
+    count++;
 
-  init_board(file_desc);
-  set_port0_direction_5812(file_desc, 0, 0);
-  set_port1_direction_5812(file_desc, 0, 0);
+    /* process the command keystroke */
+    switch (c)
+    {
 
-  test = read_status_register(file_desc);
-  printf("Status reg: 0x%02X.\n", test);
+      case 'o':       /* Open dev 0 0x0300h */
+        file_desc = open( dev_name, 0);
+        if (file_desc < 0)
+        {
+          attrset(COLOR_PAIR(1));
+          mvprintw(1,0,"Can't open device file: %s.\n", dev_name);
+        }
+        else
+        {
+          attrset(COLOR_PAIR(7));
+          mvprintw(1,0,"Device file: %s opened.",dev_name);
+          init_board(file_desc);
+        };
+        break;
 
-  test = read_dio_5812(file_desc, 0, 0);
-  printf("Digital port0: 0x%02X.\n", test);
+      case 'c':       /* Close dev 0 0x0300h */
+        close(file_desc);
+        attrset(COLOR_PAIR(7));
+        mvprintw(1,0,"Device file: %s closed.\n", dev_name);
+        break;
 
-  test = read_dio_5812(file_desc, 0, 1);
-  printf("Digital port1: 0x%02X.\n", test);
+      case 's':
+        test = read_status_register(file_desc);
+        attrset(COLOR_PAIR(3));
+        mvprintw(3,0,"Status Reg: 0x%02X.\n", test);
+        break;
 
-  // Closing dev file
-  close(file_desc);
-  printf("Device file: %s closed.\n", dev_name);
+      case 'd':       /* Config both digital ports for input */
+        set_port0_direction_5812(file_desc, 0, 0);
+        set_port1_direction_5812(file_desc, 0, 0);
+        break;
+
+      case 'r':       /* Read both digital ports */
+        attrset(COLOR_PAIR(3));
+        test = read_dio_5812(file_desc, 0, 0);
+        mvprintw(5,0,"Digital Port 0: 0x%02X.\n", test);
+        test = read_dio_5812(file_desc, 0, 1);
+        mvprintw(6,0,"Digital Port 1: 0x%02X.\n", test);
+        break;
+
+      case 'x':       /* Exit */
+        cont = FALSE;
+        break;
+    }
+  }
+
+  sleep(1);
+  finish(0);          /* we're done */
   return 0;
+}
+
+
+static void finish(int sig)
+{
+  endwin();
+  /* do your non-curses wrapup here */
+
+  exit(0);
 }
